4ac704df207a7930cbd038b03f1bc3822d57ccbc
john <john.isailofski@gmail.com>
2021-06-29 19:03:42 +1000

    Refactored to make it beautiful!

--- interpreter.go
+++ interpreter.go
@@ -9,28 +9,26 @@ import (
 	"github.com/john-isa/basic/lex"
 )
 
-func Run(text string) {
-	lexer := lex.New(text)       // create a lexer that contains the string to be parsed
-	tokens := lexer.MakeTokens() // parse the string into an array of tokens
-
-	fmt.Println(tokens)
-}
-
 func readLine(r *bufio.Reader) (string, error) {
 
 	fmt.Print("BASIC-> ")
 	return r.ReadString('\n')
 }
 
+func Run() {
+	reader := bufio.NewReader(os.Stdin) // create a reader to read from the command line
+	text, _ := readLine(reader)         // read a line of text
+	text = strings.TrimSpace(text)      // Trims all undesired spaces
+	lexer := lex.New(text)              // create a lexer that contains the string to be parsed
+	tokens := lexer.MakeTokens()        // parse the string into an array of tokens
+
+	fmt.Println(tokens)
+}
+
 //==========================================================================================
 // The main entry point
 //==========================================================================================
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 
-	text, _ := readLine(reader)
-
-	text = strings.TrimSpace(text)
-
-	Run(text)
+	Run()
 }
